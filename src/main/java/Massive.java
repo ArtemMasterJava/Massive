a3b8dbcd4e75c2803c4dd5ca3cbaab2071926549
Artem Anohin <aartem@bigmir.net>
2015-07-28 14:20:03 +0300

    add new test and realization

--- src/main/java/Massive.java
+++ src/main/java/Massive.java
@@ -6,9 +6,9 @@ public class Massive {
 
 
     public static int[] addElement(int[] myMassive, int newElement) {
-            int[] newMassive = new int[myMassive.length+1];
-            System.arraycopy(myMassive,0,newMassive,0,myMassive.length);
-            newMassive[newMassive.length-1] = newElement;
+        int[] newMassive = new int[myMassive.length + 1];
+        System.arraycopy(myMassive, 0, newMassive, 0, myMassive.length);
+        newMassive[newMassive.length - 1] = newElement;
 
 
         return newMassive;
--- src/test/java/TestMassive.java
+++ src/test/java/TestMassive.java
@@ -8,12 +8,11 @@ public class TestMassive {
     @Test
     public void addElement() {
         int[] myMassive = {1, 5, 4, 23, 65, 32, 78};
-        int  newElement = 112;
+        int newElement = 112;
         int[] expectedArray = {1, 5, 4, 23, 65, 32, 78, 112};
 
-        int[] returnValue = Massive.addElement(myMassive,newElement);
+        int[] returnValue = Massive.addElement(myMassive, newElement);
 
-        
 
         assertArrayEquals(expectedArray, returnValue);
     }
